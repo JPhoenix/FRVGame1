e3210f5e9bee22919fe21c59b1c25bea73a5ba29
JPhoenix <rochavesf@hotmail.com>
2015-03-09 00:16:17 -0600

    Debug variables deleted

--- FRVPlayer1/decentstatregy.cpp
+++ FRVPlayer1/decentstatregy.cpp
@@ -12,68 +12,59 @@ int currentColorIndex;
 unsigned char currentColor, dummyColor;
 int i;
 
-int caso = 0;
-
-void decent_strategy(int n, int k){
-	caso++;
-    remaining = k;
-    c = (unsigned char)n;
-    distinctColors = 0;
-    while (remaining && c-- != 1){
-    	memset(guess,c,k);
-    	write(socket_desc,guess,k);
-        read_answer();
-        if (gR == k) return;
-        if (gR){
-            colors[distinctColors]=c;
-            colorAmount[distinctColors]=gR;
-            distinctColors++;
-            remaining-=gR;
-        }
-    }
-    if (remaining){
-        colors[distinctColors]=c;
-        colorAmount[distinctColors]=remaining;
-        distinctColors++;
-    }
-    colorsLeft = distinctColors;
-    currentPosition = 0;
-    while (colorsLeft != 1){
-    	currentColorIndex = 0;
-        for (i = 1; i < colorsLeft; i++){
-            while (!colorAmount[currentColorIndex])currentColorIndex++;
-            currentColor = colors[currentColorIndex];
-            dummyColor = colors[currentColorIndex+1];
-            memset(guess,dummyColor,k);
-            guess[currentPosition]=currentColor;
-            write(socket_desc,guess,k);
-            read_answer();
-            if (gR == k) return;
-            if (!gN){
-            	answer[currentPosition++]=currentColor;
-            	if (! (--colorAmount[currentColorIndex])){
-            		colorsLeft--;
-            	}
-            	goto end;
-            } else if (gN == 2){
-            	answer[currentPosition++]=dummyColor;
-				if (! (--colorAmount[currentColorIndex+1])){
-					colorsLeft--;
-				}
-				goto end;
-            }
-            currentColorIndex++;
-        }
-		while (!colorAmount[currentColorIndex])currentColorIndex++;
-		answer[currentPosition++]=colors[currentColorIndex];
-		if (! (--colorAmount[currentColorIndex])){
-			colorsLeft--;
+void decent_strategy(int n, int k) {
+	remaining = k;
+	c = (unsigned char) n;
+	distinctColors = 0;
+	while (remaining && c-- != 1) {
+		memset(guess, c, k);
+		write(socket_desc, guess, k);
+		read_answer();
+		if (gR == k)
+			return;
+		if (gR) {
+			colors[distinctColors] = c;
+			colorAmount[distinctColors++] = gR;
+			remaining -= gR;
 		}
-        end:;
-    }
-    currentColorIndex=0;
-    while (!colorAmount[currentColorIndex])currentColorIndex++;
-    while (currentPosition<k) answer[currentPosition++]=colors[currentColorIndex];
-    write(socket_desc,answer,k);
-    read_answer();
+	}
+	if (remaining) {
+		colors[distinctColors] = c;
+		colorAmount[distinctColors++] = remaining;
+	}
+	colorsLeft = distinctColors;
+	currentPosition = 0;
+	while (colorsLeft != 1) {
+		currentColorIndex = 0;
+		for (i = 1; i < colorsLeft; i++) {
+			while (!colorAmount[currentColorIndex])	currentColorIndex++;
+			currentColor = colors[currentColorIndex];
+			dummyColor = colors[currentColorIndex + 1];
+			memset(guess, dummyColor, k);
+			guess[currentPosition] = currentColor;
+			write(socket_desc, guess, k);
+			read_answer();
+			if (gR == k)
+				return;
+			if (!gN) {
+				answer[currentPosition++] = currentColor;
+				if (!(--colorAmount[currentColorIndex])) colorsLeft--;
+				goto end;
+			} else if (gN == 2) {
+				answer[currentPosition++] = dummyColor;
+				if (!(--colorAmount[currentColorIndex + 1])) colorsLeft--;
+				goto end;
+			}
+			currentColorIndex++;
+		}
+		while (!colorAmount[currentColorIndex])	currentColorIndex++;
+		answer[currentPosition++] = colors[currentColorIndex];
+		if (!(--colorAmount[currentColorIndex])) colorsLeft--;
+		end: ;
+	}
+	currentColorIndex = 0;
+	while (!colorAmount[currentColorIndex])	currentColorIndex++;
+	while (currentPosition < k)	answer[currentPosition++] = colors[currentColorIndex];
+	write(socket_desc, answer, k);
+	read_answer();
 }
